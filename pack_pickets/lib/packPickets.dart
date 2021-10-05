646454216a47c619ae4001121eff70fad1f5a1c8
halka9000stg <csta19097@g.nihon-u.ac.jp>
2021-10-05 15:44:38 +0900

    mod: class instance addition

--- pack_pickets/lib/packPickets.dart
+++ pack_pickets/lib/packPickets.dart
@@ -1,8 +1,25 @@
 import "packge:pack_pickets/src/pickets.dart";
+import "packge:pack_pickets/src/creater.dart";
+import "packge:pack_pickets/src/detector.dart";
+import "packge:pack_pickets/src/publisher.dart";
+import "packge:pack_pickets/src/mover.dart";
 
 class PackPickets{
-  PPPkgsEnv env;
+  PicketsEnvEnv _env;
+  Creater _creater;
+  Detector _detector;
+  Publisher _publisher;
+  Mover _mover;
   PackPickets(){
-    this.env = PPPkgsEnv();
+    this._env = PicketsEnv();
+    this._creater = Creater(this._env);
+    this._detector = Detector(this._env);
+    this._publisher = Publisher(this._env);
+    this._mover = Mover(this._env);
   }
+  PicketsEnv get env => this._env;
+  Creater get creater => this._creater;
+  Detector get detector => this._detector;
+  Publisher get publisher => this._publisher;
+  Mover get mover => this._mover;
 }
